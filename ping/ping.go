4ddbd7511877a9ab08e8fd02c8dda7d1aec15230
Binij Shrestha <shresthabinij@gmail.com>
2023-08-08 23:33:43 +0545

    update ping

--- ping/ping.go
+++ ping/ping.go
@@ -8,7 +8,10 @@ import (
 	"net/http"
 )
 
+var Version = "1.0.0"
+
 func Ping(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Incoming request from %s for path %s", r.RemoteAddr, r.URL.Path)
-	fmt.Fprint(w, "Pong!!!")
+	response := fmt.Sprintf("Pong!!! Version: %s", Version)
+	fmt.Fprint(w, response)
 }
